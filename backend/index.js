415c3eff3c5666cae4a58b02e6b2e103cf0eb50b
Sagar-Admane <118822902+Sagar-Admane@users.noreply.github.com>
2025-01-22 22:59:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ import cors from "cors";
 
 const app = express();
 app.use(express.urlencoded({extended : true}))
-app.use(cors());
+app.use(cors({
+    origin : [""],
+    methods : ["POST", "GET"],
+    credentials : true}
+    ));
 app.use(express.json())
 
 app.get("/",(req, res)=>{
